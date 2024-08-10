1522f5cf6bbb63241cde03ef775ef3f6df679e61
rahulpinto19 <rahulyadav252424@gmail.com>
2024-08-10 22:02:46 +0530

    "easy commit"

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const port = 3001;
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+
 const connectToMongo = require("./db");
 const upload = require("./routes/uploadjob");
 const fetchjob = require("./routes/fetchjobs");
 
 // Define a basic route
+app.get("/",(req,res)=>
+{
+  res.send({message:"working"})
+})
 app.use("/", upload);
 app.use("/", fetchjob);
 
