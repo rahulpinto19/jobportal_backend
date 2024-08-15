ee745b61f61fdf64c36e242ed23fe05d36017f76
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2024-08-15 13:33:20 +0530

    Update uploadjob.js

--- routes/uploadjob.js
+++ routes/uploadjob.js
@@ -1,27 +1,24 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-app.use(express.json());
 const cors = require("cors");
-const job = require("../models/job");
+const Job = require("../models/job");
 
+app.use(express.json());
 app.use(cors());
-router.post("/upload", async (req, res) => {
-  const newjob = req.body;
-  console.log(newjob);
-  try {
-    const newJob = new job(newjob);
-    const result = await newJob.save();
-    
-    if (result) {
 
-      res.send({ code: 200, message: "job saved in the database" });
-    } else {
-      res.send({ code: 200, message: "Technical issue",result:result });
-    }
+router.post("/upload", async (req, res) => {
+  const newJobData = req.body;
+  console.log(newJobData);
+
+  try {
+    const newJob = new Job(newJobData);
+    const result = await newJob.save();
+    console.log(result);
+    res.status(200).send({ code: 200, message: "Job saved in the database", result });
   } catch (err) {
-    console.log(err)
-    res.send({ code: 200, message: "Technical issue",err:err });
+    console.error("Error saving job:", err);
+    res.status(500).send({ code: 500, message: "Technical issue", error: err.message });
   }
 });
 
