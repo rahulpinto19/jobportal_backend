5c16e46f1f949b5aac038de3abfc84be993d31cf
manuka rahul <96047526+rahulpinto19@users.noreply.github.com>
2024-08-15 13:32:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express();
 const port = 3002;
 app.use(express.json());
 const cors = require("cors");
-
-
-app.use(cors({
-    origin: ['https://jobportal-kk6v.vercel.app'],
+// app.use(cors())
+app.use(
+  cors({
+    origin: ["https://jobportal-kk6v.vercel.app"],
     credentials: true,
-    sameSite: 'none'
-}));
+    sameSite: "none",
+  })
+);
 const connectToMongo = require("./db");
 const upload = require("./routes/uploadjob");
 const fetchjob = require("./routes/fetchjobs");
 
 // Define a basic route
-app.get("/",(req,res)=>
-{
-  res.send({message:working})
-})
+app.get("/", (req, res) => {
+  res.send({ message: working });
+});
 app.use("/", upload);
 app.use("/", fetchjob);
 
